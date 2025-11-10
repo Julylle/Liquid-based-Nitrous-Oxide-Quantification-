99c186e18a0dc0aa7568f89d56dab881c2eca0cb
Shuting Wang <58683838+Julylle@users.noreply.github.com>
2025-11-10 15:25:42 +1000

    Add files via upload

--- demo.py
+++ demo.py
@@ -7,7 +7,9 @@ Created on Mon Nov 10 13:20:01 2025
 #%%
 from liquidbased_quantification import LiquidQuantifier
 
-model = LiquidQuantifier("data/Monitoring_Data_Example.xlsx")
+# The D_R and AerationFieldSize parameters can be adjusted based on specific site conditions
+model = LiquidQuantifier("data/Monitoring_Data_Example.xlsx", D_R = 7.55, AerationFieldSize = 462.0)
+
 results = model.run_pipeline(show_plots=True)
 print(results.head())
 results.to_excel("liquid_quantification_results.xlsx", index=False)
